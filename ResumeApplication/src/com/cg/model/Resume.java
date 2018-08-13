978a7d10d68b48de4e619d0c3cfe6a9f91492476
manasabyndoor <41484448+manasabyndoor@users.noreply.github.com>
2018-08-13 07:48:18 +0530

    Update Resume.java

--- ResumeApplication/src/com/cg/model/Resume.java
+++ ResumeApplication/src/com/cg/model/Resume.java
@@ -1,38 +1,63 @@
-package com.cg.model;
+package com.resume.bean;
 
 public class Resume {
-private String firstName;
-private String lastName;
-private String gender;
-private long contact;
-
-public Resume() {
-	// TODO Auto-generated constructor stub
-}
-public String getFirstName() {
-	return firstName;
-}
-public void setFirstName(String firstName) {
-	this.firstName = firstName;
-}
-public String getLastName() {
-	return lastName;
-}
-public void setLastName(String lastName) {
-	this.lastName = lastName;
-}
-public String getGender() {
-	return gender;
-}
-public void setGender(String gender) {
-	this.gender = gender;
-}
-public long getContact() {
-	return contact;
-}
-public void setContact(long contact) {
-	this.contact = contact;
-}
-
+	private String firstName,lastName,gender,qualification,dob,hobbies,address,email,weburl,contact,description;
+	private String[] skills;
+	
+	
+	public Resume(String firstName, String lastName, String gender, String qualification, String dob, String hobbies,
+			String address, String email, String weburl, String contact, String description, String[] skills) {
+		
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gender = gender;
+		this.qualification = qualification;
+		this.dob = dob;
+		this.hobbies = hobbies;
+		this.address = address;
+		this.email = email;
+		this.weburl = weburl;
+		this.contact = contact;
+		this.description = description;
+		this.skills = skills;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public String getQualification() {
+		return qualification;
+	}
+	public String getDob() {
+		return dob;
+	}
+	public String getHobbies() {
+		return hobbies;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public String getWeburl() {
+		return weburl;
+	}
+	public String getContact() {
+		return contact;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public String[] getSkills() {
+		return skills;
+	}
+	
+	
 
 }
